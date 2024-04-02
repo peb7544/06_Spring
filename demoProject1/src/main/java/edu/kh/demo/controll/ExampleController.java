5baf1c9c2e611277ac16d7776aa7379d840aa888
peb7544 <peb7544@naver.com>
2024-04-02 16:21:59 +0900

    spring

---
+++ demoProject1/src/main/java/edu/kh/demo/controll/ExampleController.java
@@ -0,0 +1,35 @@
+package edu.kh.demo.controll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Bean : 스프링이 만들고 관리하는 객체
+
+@Controller // 요청/응답 제어 역할인 Controller 임을 명시 + Bean 등록
+public class ExampleController {
+	
+	/* 요청 주소 매핑하는 방법
+	 * 
+	 * 1) @Requestmappning("주소")
+	 * 
+	 * 2) @Getmapping("주소") : GET(조회) 방식 요청 매핑
+	 * 
+	 *    @Postmapping("주소") : POST(삽입) 방식 요청 매핑
+	 *    
+	 *    @PutMapping("주소") : PUT(수정) 방식 요청 매핑
+	 *    
+	 *    @DeleteMapping("주소") : DELETE(삭제) 방식 요청 매핑
+	 * */
+	
+	@GetMapping("example") // /example GET 방식 요청 매칭
+	public String exampleMehod() {
+		
+		// forward하려는 html 파일 경로 작성
+		// 단, ViewResolver가 제공하는
+		// Thymeleaf 접두사, 접미사 제외하고 작성
+		
+		// 접두사 : classpath:/templates/
+		// 접미사 : .html
+		return "example"; // src/main/resources/templates/example.html
+	}
+}
