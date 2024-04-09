8c0eda0ebb94820fc5e3b04d4e4d28d23e8fb35c
peb7544 <peb7544@naver.com>
2024-04-09 17:03:22 +0900

    spring

--- todoList-boot/src/main/java/edu/kh/todo/controller/AjaxController.java
+++ todoList-boot/src/main/java/edu/kh/todo/controller/AjaxController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -117,6 +118,20 @@ public class AjaxController {
 	@DeleteMapping("delete")
 	public int todoDelete(@RequestBody int todoNo) {
 		return service.todoDelete(todoNo);
+	}
+	
+	// 완료 여부 변경
+	@ResponseBody
+	@PutMapping("changeComplete")
+	public int changeComplete(@RequestBody Todo todo) {
+		return service.changeComplete(todo);
+	}
+	
+	// 할 일 수정
+	@ResponseBody
+	@PutMapping("update")
+	public int todoUpdate(@RequestBody Todo todo) {
+		return service.todoUpdate(todo);
 	}
 	
 }
