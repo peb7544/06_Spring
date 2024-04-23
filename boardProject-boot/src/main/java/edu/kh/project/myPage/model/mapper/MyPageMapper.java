d86e172c781806f8a4b52f8e545c8109d2e82cc3
peb7544 <peb7544@naver.com>
2024-04-23 17:47:41 +0900

    spring

--- boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
@@ -1,18 +1,19 @@
 package edu.kh.project.myPage.model.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.myPage.model.dto.UploadFile;
 
 @Mapper
 public interface MyPageMapper {
-
 	/** 회원 정보 수정
 	 * @param inputMember
 	 * @return result
-	 */
+	 * */
 	int updateInfo(Member inputMember);
 
 	/** 회원의 비밀번호 조회
@@ -32,5 +33,22 @@ public interface MyPageMapper {
 	 * @return result
 	 */
 	int secession(int memberNo);
+
+	/** 파일 정보를 DB에 삽입
+	 * @param uf
+	 * @return result
+	 */
+	int insertUploadFile(UploadFile uf);
+
+	/** 파일 목록 조회
+	 * @return
+	 */
+	List<UploadFile> fileList();
+
+	/** 프로필 이미지 변경
+	 * @param mem
+	 * @return
+	 */
+	int profile(Member mem);
 
 }
--- boardProject-boot/src/main/java/edu/kh/project/myPage/model/service/MyPageService.java
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/service/MyPageService.java
@@ -1,10 +1,12 @@
 package edu.kh.project.myPage.model.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.multipart.MultipartFile;
 
 import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.myPage.model.dto.UploadFile;
 
 public interface MyPageService {
 
@@ -34,5 +36,32 @@ public interface MyPageService {
 	 * @return path
 	 */
 	String fileUpload1(MultipartFile uploadFile) throws Exception;
+
+	/** 파일 업로드 테스트 2(+DB)
+	 * @param uploadFile
+	 * @param memberNo
+	 * @return result
+	 */
+	int fileUpload2(MultipartFile uploadFile, int memberNo) throws Exception;
+
+	/** 파일 목록 조회
+	 * @return
+	 */
+	List<UploadFile> fileList();
+
+	/** 여러 파일 업로드
+	 * @param aaaList
+	 * @param bbbList
+	 * @param memberNo
+	 * @return
+	 */
+	int fileUpload3(List<MultipartFile> aaaList, List<MultipartFile> bbbList, int memberNo) throws Exception;
+
+	/** 프로필 이미지 변경
+	 * @param profileImg
+	 * @param loginMember
+	 * @return
+	 */
+	int profile(MultipartFile profileImg, Member loginMember) throws Exception;
 
 }
