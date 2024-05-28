001e8dd8834e973d50911d117879dc0982651612
peb7544 <peb7544@naver.com>
2024-05-28 09:02:34 +0900

    240508 spring

--- boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,12 +1,19 @@
 package edu.kh.project.main.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@PropertySource("classpath:/config.properties")
 public class MainController {
+	
+	@Value("${my.public.data.service.key.decode}")
+	private String decodeServiceKey;
 	
 	@RequestMapping("/") // "/" 요청 매핑(method 가리지 않음)
 	public String mainPage() {
@@ -23,5 +30,15 @@ public class MainController {
 		ra.addFlashAttribute("message", "로그인 후 이용해주세요~");
 		
 		return "redirect:/";
+	}
+	
+	/* 공공데이터 
+	 * 서비스키 리턴하기
+	 * */
+	@GetMapping("/getServiceKey")
+	@ResponseBody
+	public String getServiceKey() {
+		
+		return decodeServiceKey;
 	}
 }
